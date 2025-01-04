d54a8f69ae02b84ff6089dd0197c33f03cc1ae97
HadiyaKrishna16 <141121819+HadiyaKrishna16@users.noreply.github.com>
2025-01-04 09:05:33 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,24 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { RedirectToSignIn } from "@clerk/clerk-react";
 import Home from "./pages/Home";
-import AuthRoutes from "./components/AuthRoutes";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import AuthRoutes from "./components/AuthRoutes"; 
 import CreateBlog from './components/CreateBlog';
+import BlogDetail from './components/Blog/BlogDetail';
+import UserProfile from './components/User/UserProfile';
 
 const App = () => {
   return (
-    
+
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/protected" element={<RedirectToSignIn />} />
       <Route path="/*" element={<AuthRoutes />} />
       <Route path="/create-blog" element={<CreateBlog />} />
-     
+      <Route path="/blog/:id" element={<BlogDetail />} />
+      <Route path="/user/:userId" element={<UserProfile />} />
+
     </Routes>
-    
+
 
 
   );

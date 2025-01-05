d7c387f386253fe40fe3ed6f6a1536c090cdd9e6
HadiyaKrishna16 <141121819+HadiyaKrishna16@users.noreply.github.com>
2025-01-05 17:15:42 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,46 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { RedirectToSignIn } from "@clerk/clerk-react";
 import Home from "./pages/Home";
 import AuthRoutes from "./components/AuthRoutes"; 
 import CreateBlog from './components/CreateBlog';
 import BlogDetail from './components/Blog/BlogDetail';
 import UserProfile from './components/User/UserProfile';
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
   return (
-
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/protected" element={<RedirectToSignIn />} />
-      <Route path="/*" element={<AuthRoutes />} />
-      <Route path="/create-blog" element={<CreateBlog />} />
-      <Route path="/blog/:id" element={<BlogDetail />} />
-      <Route path="/user/:userId" element={<UserProfile />} />
-
-    </Routes>
-
-
-
+    <>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: '#333',
+            color: '#fff',
+            borderRadius: '10px',
+          },
+          success: {
+            iconTheme: {
+              primary: '#4CAF50',
+              secondary: '#fff',
+            },
+          },
+          error: {
+            iconTheme: {
+              primary: '#f44336',
+              secondary: '#fff',
+            },
+          },
+        }}
+      />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/*" element={<AuthRoutes />} />
+        <Route path="/create-blog" element={<CreateBlog />} />
+        <Route path="/blog/:id" element={<BlogDetail />} />
+        <Route path="/user/:userId" element={<UserProfile />} />
+      </Routes>
+    </>
   );
 }
 
